2aeb407b0132aa01c33e960aade56bf6762a11be
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-09 15:22:30 +0000

    UPDATE(main.py).

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sources import GameMatrix
+
 game_matrix = GameMatrix()
 game_matrix.init_winning_probs()
 probs_df = game_matrix.probs_to_df()
-(probs_df.to_csv('winning_probs.csv'))
+(probs_df.to_csv("winning_probs.csv"))
 print(probs_df)
