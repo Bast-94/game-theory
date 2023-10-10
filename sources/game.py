896752b86e1cf142b190cd14f9bb03f3b44bda9e
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 18:30:33 +0000

    UPDATE(sources/game.py): Ajout du mode verbose reglable sur False ou True.

--- sources/game.py
+++ sources/game.py
@@ -4,7 +4,7 @@ from sources.game_matrix import GameMatrix
 
 
 class Game:
-    def __init__(self, player_1=None, player_2=None) -> None:
+    def __init__(self, player_1=None, player_2=None, verbose=True) -> None:
         from sources.player import Player
 
         self.game_matrix = GameMatrix()
@@ -15,6 +15,7 @@ class Game:
         self.player_2 = Player(self) if player_2 is None else player_2
         self.player_2.game = self
         self.winner = None
+        self.verbose = verbose
 
     def render(self):
         print("Player 1: ", self.player_1.score)
@@ -26,21 +27,21 @@ class Game:
         pass
 
     def dual_phase(self):
-        print("Payer 1 subset: ", self.player_1.subset)
-        print("Payer 2 subset: ", self.player_2.subset)
         self.player_1.play_strategy()
         self.player_2.play_strategy()
-        print("Player 1 played: ", self.player_1.played_strategy)
-        print("Player 2 played: ", self.player_2.played_strategy)
+
         strat_1 = self.player_1.played_strategy
         strat_2 = self.player_2.played_strategy
-        print("Strat 1: ", strat_1)
 
         player_1_score = self.game_matrix.get_score(strat_1, strat_2)
         player_2_score = self.game_matrix.get_score(strat_2, strat_1)
-
-        print("Player 1 score: ", player_1_score)
-        print("Player 2 score: ", player_2_score)
+        if self.verbose:
+            print("Payer 1 subset: ", self.player_1.subset)
+            print("Payer 2 subset: ", self.player_2.subset)
+            print("Player 1 played: ", self.player_1.played_strategy)
+            print("Player 2 played: ", self.player_2.played_strategy)
+            print("Player 1 score: ", player_1_score)
+            print("Player 2 score: ", player_2_score)
 
         if player_1_score > player_2_score:
             print("Player 1 win")
@@ -53,6 +54,7 @@ class Game:
             print("Draw")
 
     def check_win_phase(self):
+        stop_game = False
         if not ("N" in self.player_1.subset):
             self.winner = self.player_1
             stop_game = True
