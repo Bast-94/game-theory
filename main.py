9a23a5179bb05107d6b91c70e6ec429300b60545
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 17:43:58 +0000

    UPDATE(main.py):

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ from sources import GameMatrix
 game_matrix = GameMatrix()
 game_matrix.init_winning_probs()
 mat = game_matrix.dice_matrix - game_matrix.dice_matrix.T
+print(mat)
 print(np.where(mat >= 0))
