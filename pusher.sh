0d3db2019630e705ecb12d36a597c63b69714019
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 16:07:03 +0000

    UPDATE(.gitignore): Ajout du opt.sh dans le git ignore

--- pusher.sh
+++ pusher.sh
@@ -1,8 +1,13 @@
-echo $(git status --porcelain  | awk 'match($1, ""){print $2}' )
-for file in $(git status --porcelain  | awk 'match($1, ""){print $2}' );
-    do
-        git add $file 2> /dev/null
-        echo "Commiting $file"
-        read msg
-        git commit -m "UPDATE($file): $msg"
-    done
+function pusher
+{
+    echo $(git status --porcelain  | awk 'match($1, ""){print $2}' )
+    for file in $(git status --porcelain  | awk 'match($1, ""){print $2}' );
+        do
+            git add $file 2> /dev/null
+            echo "Commiting $file"
+            read msg
+            git commit -m "UPDATE($file): $msg"
+        done
+}
+pusher
+
