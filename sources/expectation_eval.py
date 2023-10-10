f54c549a1f309b1c63c88dc84b90cfc00d27e40a
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 17:26:31 +0000

    UPDATE(sources/expectation_eval.py): Correction du choice

--- sources/expectation_eval.py
+++ sources/expectation_eval.py
@@ -1,3 +1,5 @@
+import random
+
 from sources.strategy import Strategy
 
 
@@ -5,7 +7,16 @@ class ExpectationEval(Strategy):
     def __init__(self, player) -> None:
         super().__init__(player)
 
+    def choice(self):
+        return random.choice(self.get_subset())
+
     def create_subset(self):
         subset = ["N"]
-        self.game_matrix.init_winning_probs()
-        best_strat_index = self.game_matrix.winning_probs.mean(axis=1).argmax()
+        self.get_game_matrix().init_winning_probs()
+        best_strat_index = self.get_game_matrix().winning_probs.mean(axis=1).argmax()
+        best_strat = self.get_game_matrix().strategy_table[best_strat_index]
+        print(f"Best strat: {best_strat}")
+        subset = ["N"]
+        for _ in range(1, self.get_game().subset_size):
+            subset.append(best_strat)
+        self.player.subset = subset
