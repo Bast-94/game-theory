8656ce626096d8bc35644076d45a308867adacdd
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 17:41:16 +0000

    UPDATE(sources/expectation_eval.py):

--- sources/expectation_eval.py
+++ sources/expectation_eval.py
@@ -7,12 +7,9 @@ class ExpectationEval(Strategy):
     def __init__(self, player) -> None:
         super().__init__(player)
 
-    def choice(self):
-        return random.choice(self.get_subset())
-
     def create_subset(self):
         subset = ["N"]
-        self.get_game_matrix().init_winning_probs()
+        self.get_game_matrix().init_winning_probs(count_draws=True)
         best_strat_index = self.get_game_matrix().winning_probs.mean(axis=1).argmax()
         best_strat = self.get_game_matrix().strategy_table[best_strat_index]
         print(f"Best strat: {best_strat}")
