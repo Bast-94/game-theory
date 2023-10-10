b08a36c71f22d37273526032063d4c50b9a280f5
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 17:25:02 +0000

    UPDATE(sources/__init__.py): Ajout de toutes les sources

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,7 +1,15 @@
 from sources.default_strategy import DefaultStrategy
+from sources.expectation_eval import ExpectationEval
 from sources.game import Game
 from sources.game_matrix import GameMatrix
 from sources.player import Player
 from sources.strategy import Strategy
 
-__all__ = ["GameMatrix", "Game", "Player"]
+__all__ = [
+    "GameMatrix",
+    "Game",
+    "Player",
+    "Strategy",
+    "DefaultStrategy",
+    "ExpectationEval",
+]
