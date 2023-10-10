bbe884c5405d9e397e51bd7c3c2ce694887f2cc2
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 16:18:50 +0000

    UPDATE(sources/default_strategy.py):

--- sources/default_strategy.py
+++ sources/default_strategy.py
@@ -1,16 +1,15 @@
-from sources.strategy import Strategy
-
 import random
 
+from sources.strategy import Strategy
+
+
 class DefaultStrategy(Strategy):
-    
     def __init__(self, player) -> None:
         super().__init__(player)
-    
 
     def choice(self):
         pass
-    
+
     def create_subset(self):
         subset = ["N"]
         for _ in range(1, self.game_matrix.n_strategies):
