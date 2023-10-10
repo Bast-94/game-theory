ba65d1b1dd113b57d3eb189d66a6caa404f8a8e6
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 13:42:15 +0000

    UPDATE(pusher.sh): Changing pusher.sh in order to add particular commit msg

--- pusher.sh
+++ pusher.sh
@@ -2,5 +2,7 @@ echo $(git status --porcelain  | awk 'match($1, ""){print $2}' )
 for file in $(git status --porcelain  | awk 'match($1, ""){print $2}' );
     do
         git add $file 2> /dev/null
-        git commit -m "UPDATE($file)."
+        echo "Commiting $file"
+        read msg
+        git commit -m "UPDATE($file): $msg"
     done
