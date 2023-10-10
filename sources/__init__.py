696d27b3a65fcbec6f91c8f49c8890d4b79df5a3
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 16:07:30 +0000

    UPDATE(sources/__init__.py): add default strategy and strategy

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,5 +1,8 @@
 from sources.game import Game
 from sources.game_matrix import GameMatrix
 from sources.player import Player
+from sources.strategy import Strategy
+from sources.default_strategy import DefaultStrategy
+
 
 __all__ = ["GameMatrix", "Game", "Player"]
