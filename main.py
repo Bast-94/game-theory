e522875d117e4fcbb8f73144da270cb082d3c1e0
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 17:53:59 +0000

    UPDATE(main.py):

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ game_matrix = GameMatrix()
 game_matrix.init_winning_probs()
 mat = game_matrix.dice_matrix - game_matrix.dice_matrix.T
 print(mat)
-print(mat[np.where(mat > 0)])
+couples = np.array(np.where(mat > 0))
+couples = couples.T
+print(couples)
+for a, b in couples:
+    print(game_matrix.strategy_table[a], ">", game_matrix.strategy_table[b])
+
+    print("----")
