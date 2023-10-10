d615ae7787900ffc97d42e89116c9b6892f6223e
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 16:18:49 +0000

    UPDATE(sources/__init__.py):

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,8 +1,7 @@
+from sources.default_strategy import DefaultStrategy
 from sources.game import Game
 from sources.game_matrix import GameMatrix
 from sources.player import Player
 from sources.strategy import Strategy
-from sources.default_strategy import DefaultStrategy
-
 
 __all__ = ["GameMatrix", "Game", "Player"]
