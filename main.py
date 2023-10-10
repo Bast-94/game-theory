17406d80fdfe60ef3e26d0e7af915dd99098044d
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 10:28:43 +0000

    UPDATE(main.py).

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from sources import Game, GameMatrix, Player
 
 game = Game()
 game.render()
-game_matrix = GameMatrix()
+game_matrix = game.game_matrix
 game_matrix.init_winning_probs()
+
 print(game_matrix.probs_to_df())
+game.run()
