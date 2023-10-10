fdf84709a7bbf7eafe567de7f08ee813236cb8c5
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 17:42:29 +0000

    UPDATE(main.py):

--- main.py
+++ main.py
@@ -2,6 +2,4 @@ from sources import GameMatrix
 
 game_matrix = GameMatrix()
 game_matrix.init_winning_probs()
-print(game_matrix.winning_probs.mean(axis=1))
-print(game_matrix.winning_probs)
-# game_matrix.winning_probs
+print(game_matrix.dice_matrix - game_matrix.dice_matrix.T)
