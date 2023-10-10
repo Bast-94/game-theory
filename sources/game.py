33682d2d5b6021d28c15c567c90f4af84d1fa837
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 09:56:26 +0000

    UPDATE(sources/game.py).

--- sources/game.py
+++ sources/game.py
@@ -1,10 +1,19 @@
-from sources import GameMatrix
+from sources import GameMatrix, Player
 
 
 class Game:
     def __init__(self) -> None:
         self.game_matrix = GameMatrix()
         self.game_matrix.init_winning_probs()
+        self.subset_size = 10
+        self.player_1 = Player(self)
+        self.player_2 = Player(self)
+
+    def render(self):
+        print("Player 1: ", self.player_1.score)
+        print("Player 2: ", self.player_2.score)
+        print("Player 1 subset: ", self.player_1.subset)
+        print("Player 2 subset: ", self.player_2.subset)
 
     def buy_phase(self):
         pass
