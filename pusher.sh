e02cf7c410a10937814795a8180f25fe0c1e41f8
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 16:07:13 +0000

    UPDATE(pusher.sh): Refractor

--- pusher.sh
+++ pusher.sh
@@ -1,5 +1,7 @@
 function pusher
 {
+    # if $1 is not empty
+    
     echo $(git status --porcelain  | awk 'match($1, ""){print $2}' )
     for file in $(git status --porcelain  | awk 'match($1, ""){print $2}' );
         do
