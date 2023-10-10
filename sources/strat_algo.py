aac63591bd6b33888e561dfb418d6111477be994
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 13:43:13 +0000

    UPDATE(sources/strat_algo.py): Fixing get_subset

--- sources/strat_algo.py
+++ sources/strat_algo.py
@@ -10,8 +10,8 @@ class StratAlgo:
         return self.player.subset
 
     def get_subset_size(self, player: Player):
-        return len(self.get_subset(player))
+        return len(self.get_subset())
     
     def choice(self, player: Player):
-        return self.get_subset(player)[0]
+        return self.get_subset()[0]
     
