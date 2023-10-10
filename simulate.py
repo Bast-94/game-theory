2ae4efab22a4828a45d916982c7e7148ba89bfae
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 17:41:17 +0000

    UPDATE(simulate.py):

---
+++ simulate.py
@@ -0,0 +1,11 @@
+from sources import (DefaultStrategy, ExpectationEval, Game, GameMatrix,
+                     Player, Strategy)
+
+player_1 = Player(Game(), ExpectationEval)
+game = Game(player_1=player_1)
+game.render()
+game_matrix = game.game_matrix
+game_matrix.init_winning_probs()
+
+print(game_matrix.probs_to_df())
+game.run()
