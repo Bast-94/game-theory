ca19c1d79a2d6cb96d639bce4f9b8a4d409a9e14
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 19:19:35 +0000

    UPDATE(main.py):

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import numpy as np
 
 from sources import GameMatrix
@@ -5,11 +7,23 @@ from sources import GameMatrix
 game_matrix = GameMatrix()
 game_matrix.init_winning_probs()
 mat = game_matrix.dice_matrix - game_matrix.dice_matrix.T
-print(mat)
+# print(mat)
 couples = np.array(np.where(mat > 0))
 couples = couples.T
-print(couples)
-for a, b in couples:
-    print(game_matrix.strategy_table[a], ">", game_matrix.strategy_table[b])
+# print(couples)
+dict_with_lists = {"A": [1, 2], "B": ["x", "y"], "C": [True, False]}
 
-    print("----")
+
+def gen_list_of_dicts(dict_with_lists):
+    # Utilisez product pour obtenir une liste de dictionnaires
+    list_of_dicts = [
+        dict(zip(dict_with_lists.keys(), values))
+        for values in product(*dict_with_lists.values())
+    ]
+    return list_of_dicts
+
+
+# 'list_of_dicts' contiendra la liste de dictionnaires souhaitée
+#
+list_of_dicts = gen_list_of_dicts(dict_with_lists)
+print(list_of_dicts)
