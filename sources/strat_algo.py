3a36341bc496c9cc69cc855e3adf06ad84388cfb
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-10 12:02:15 +0000

    ADD(StratAlgo).

--- sources/strat_algo.py
+++ sources/strat_algo.py
@@ -4,3 +4,14 @@ from sources.player import Player
 class StratAlgo:
     def __init__(self, player: Player) -> None:
         self.player = player
+        self.game_matrix = player.game.game_matrix
+    
+    def get_subset(self):
+        return self.player.subset
+
+    def get_subset_size(self, player: Player):
+        return len(self.get_subset(player))
+    
+    def choice(self, player: Player):
+        return self.get_subset(player)[0]
+    
